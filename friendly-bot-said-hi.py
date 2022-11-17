242a8cf2d7e815c90bdd04de052c3d94e45c926b
Abdullah <aai27@txstate.edu>
2022-11-17 00:09:33 -0600

    updated html and py files

--- friendly-bot-said-hi.py
+++ friendly-bot-said-hi.py
@@ -4,8 +4,14 @@ import json
 
 app = flask.Flask(__name__)
 
+def insult_generator():
+    insult = requests.get("https://evilinsult.com/generate_insult.php?lang=en&type=text")
+    
+    return insult.text
+
 @app.route('/')
 def main():
-    return flask.render_template('chat-bot.html')
+    insult = insult_generator()
+    return flask.render_template('chat-bot.html', insult = insult)
 
 app.run(debug=True)
